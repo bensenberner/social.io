3149b3799e013711cbe595c17b46acd3ce7f6f9a
Ben Lerner <benlerner95@gmail.com>
2015-06-06 19:50:30 -0400

    coming up on something hot

--- hp.py
+++ hp.py
@@ -1,22 +1,32 @@
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, render_template
 from iodpython.iodindex import IODClient
 import json
 
 app = Flask(__name__)
-client = IODClient("http://api.idolondemand.com", "d1a8fb96-f5be-482a-bd9d-c8e63d096338")
-
+client = IODClient("http://api.idolondemand.com/","d1a8fb96-f5be-482a-bd9d-c8e63d096338")
 f = open('harry.txt', 'r')
-words = f.read()
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/senti', methods=['POST'])
 def index():
-    if request.method == 'GET':
-        r = client.post('analyzesentiment',{'text':'Your mother is a whore'})
-        return r
-    else:
-        return "fluck you"
-        #return request.get_data()
+    #r = client.post('analyzesentiment',{'text':str(request.get_data())})
+    r = client.post('analyzesentiment',{'text':f.read()})
+    myjson = r.json()
+    friendly = False if myjson['aggregate']['score'] < 0 else True
+    listOfTopics = getListOfTopics(myjson)
+    return json.dumps({"friendly":friendly, "topics":listOfTopics})
 
+def getListOfTopics(myjson):
+    topics = []
+    happyThoughts = myjson['positive']
+    for thought in happyThoughts:
+        topic = thought['topic']
+        if topic is not None:
+            topicBreakdown = topic.split(' ')
+            if len(topicBreakdown) < 4:
+                topics.append(topic)
+    # Get them unique without using numpy bullshit
+    topics = list(set(topics))
+    return topics
 
 if __name__ == '__main__':
     app.run(debug=True)
